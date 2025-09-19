4682c7b60e77032ffffb7b5109d447a3bf351e7d
Madu David <madu@powerlabstech.com>
2025-09-19 20:34:39 +0100

    Update codeBlock.tsx

--- src/app/resources/outlet/blog/posts/[post]/codeBlock.tsx
+++ src/app/resources/outlet/blog/posts/[post]/codeBlock.tsx
@@ -10,6 +10,17 @@ import 'prismjs/plugins/toolbar/prism-toolbar.min.js';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js';
 
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-tsx';
+
+// Extra languages
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-rust';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-markup';
 import ReactMarkdown from 'react-markdown';
