3004d54c764af8c4b7f6cb57f2a2c44a1615a973
Madu David <madu@powerlabstech.com>
2025-09-22 07:34:39 +0100

    Update page.tsx

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -23,7 +23,7 @@ export default function Careers() {
         title="Break Things. Responsibly"
         subtitle="Stay in the loop on career opportunities and company updates."
         newsLetterType={NEWS_LETTERS_TYPE.CAREERS}
-        buttonText="Join the Blue List"
+        buttonText="Subscribe"
       />
       <Container>
         <Footer />
